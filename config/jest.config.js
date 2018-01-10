0877c409ea6e4fcc2bfa4f2a0cca8b1177b13b3d
Rafał Pitoń <kontakt@rpiton.com>
2018-01-11 00:53:57 +0100

    fix jest.js setup

--- config/jest.config.js
+++ config/jest.config.js
@@ -12,18 +12,7 @@ module.exports = {
   moduleNameMapper: {
     '^misago/(.*)$': path.resolve(baseDir, "js/$1")
   },
-  globals: {
-    gettext: function(msgid) { return msgid },
-    ngettext: function(singular, plural, count) { return (count == 1) ? singular : plural },
-    gettext_noop: function(msgid) { return msgid },
-    pgettext: function(context, msgid) { return msgid },
-    npgettext: function(context, singular, plural, count) { return (count == 1) ? singular : plural },
-    interpolate: function(fmt, obj, named) {
-      if (named) {
-        return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])})
-      } else {
-        return fmt.replace(/%s/g, function(match){return String(obj.shift())})
-      }
-    }
-  }
+  setupFiles: [
+    './jest.setup'
+  ]
 }
